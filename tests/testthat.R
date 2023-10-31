eb03a53146983d3c0fe54b1d5bd0e76008efbeb5
Matt Secrest <secrestm@gene.com>
2023-10-31 07:15:59 -0700

    rename (#3)

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(TreatmentSequence)
+library(ggTreatmentSequence)
 
-test_check("TreatmentSequence")
+test_check("ggTreatmentSequence")
